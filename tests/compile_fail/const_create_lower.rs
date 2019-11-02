9cb61fa3066b1904ebafee2f0ecf8039258d382f
Nicolas Poelen <powlpy@gmail.com>
2019-11-02 17:56:55 +0100

    fix tests for compilation in 1.31.1

--- tests/compile_fail/const_create_lower.rs
+++ tests/compile_fail/const_create_lower.rs
@@ -4,6 +4,6 @@ use numid::numid;
 numid!(struct Test -> 100);
 
 fn main() {
-    const _: Test = Test::const_create_lower(101); //~ ERROR any use of this value will cause an error
+    const _CONST: Test = Test::const_create_lower(101); //~ ERROR any use of this value will cause an error
 }
  
